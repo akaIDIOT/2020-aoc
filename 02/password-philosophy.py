82e9f2041e738f3d22eac5f1505a610c967541c4
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2020-12-02 09:41:05 +0100

    Implement day 2 part 2

--- 02/password-philosophy.py
+++ 02/password-philosophy.py
@@ -3,17 +3,23 @@ import re
 
 def passwords(fname):
     pattern = re.compile(r'(?P<min>\d+)-(?P<max>\d+) (?P<letter>[a-z]): (?P<password>[a-z]+)')
-    with open('input', 'r') as passwords:
+    with open(fname, 'r') as passwords:
         for line in passwords:
-            min, max, letter, password = pattern.match(line).groups()
-            yield int(min), int(max), letter, password
+            pos1, pos2, letter, password = pattern.match(line).groups()
+            yield int(pos1), int(pos2), letter, password
 
 
 if __name__ == '__main__':
-    num_valid = 0
-    for min, max, letter, password in passwords('input'):
-        num = password.count(letter)
-        if min <= num <= max:
-            num_valid += 1
+    num_valid_rental = 0
+    num_valid_toboggan = 0
 
-    print('number of valid passwords:', num_valid)
+    for pos1, pos2, letter, password in passwords('input'):
+        num = password.count(letter)
+        if pos1 <= num <= pos2:
+            num_valid_rental += 1
+
+        if (password[pos1 - 1] == letter) ^ (password[pos2 - 1] == letter):
+            num_valid_toboggan += 1
+
+    print('number of valid sled rental passwords:', num_valid_rental)
+    print('number of valid toboggan passwords:', num_valid_toboggan)
