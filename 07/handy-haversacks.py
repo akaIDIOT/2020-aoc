6c3c68b17c1ecf02fe45c8b40ce8260a3d9c204c
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2020-12-07 20:46:09 +0100

    Implement day 7 part 2

--- 07/handy-haversacks.py
+++ 07/handy-haversacks.py
@@ -11,21 +11,33 @@ def parse_rule(line):
     return outer, inners
 
 
+def num_bags(forward, color):
+    if not forward.get(color):
+        return 0
+
+    inners = forward.get(color)
+    return sum(inners.values()) + sum((num * num_bags(forward, inner)) for inner, num in inners.items())
+
+
 if __name__ == '__main__':
-    requirements = defaultdict(set)
+    reverse = defaultdict(set)
+    forward = {}
 
     with open('input', 'r') as in_file:
         for line in in_file:
-            inner, outers = parse_rule(line.strip())
-            for outer, _ in outers.items():
-                requirements[outer].add(inner)
+            outer, inners = parse_rule(line.strip())
+            forward[outer] = inners
+            for inner, num in inners.items():
+                reverse[inner].add(outer)
 
     start = 'shiny gold'
-    known = set(requirements[start])
+    known = set(reverse[start])
     seen = set()
     while fringe := known - seen:
         for bag in fringe:
-            known.update(requirements[bag])
+            known.update(reverse[bag])
             seen.add(bag)
 
     print(f'Number of bags that contain at least one {start} bag: {len(known)}')
+
+    print(f'Total number of bags inside the {start} bag: {num_bags(forward, start)}')
