de3d7e5f071a35c99750054f060ec97405aef840
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2020-12-19 19:41:16 +0100

    Implement day 8 part 2

--- 08/handheld-halting.py
+++ 08/handheld-halting.py
@@ -1,10 +1,18 @@
-def run_to_loop(program):
+class LoopError(ValueError):
+    def __init__(self, accumulator):
+        self.accumulator = accumulator
+
+
+def run(program):
     accumulator = 0
     program_counter = 0
 
     visited = set()
 
-    while program_counter not in visited:
+    while program_counter < len(program):
+        if program_counter in visited:
+            raise LoopError(accumulator)
+
         instruction, operand = program[program_counter]
         visited.add(program_counter)
 
@@ -21,11 +29,28 @@ def run_to_loop(program):
     return accumulator
 
 
+def patch(program):
+    for idx, (instruction, operand) in enumerate(program):
+        if instruction == 'nop':
+            yield program[:idx] + [('jmp', operand)] + program[idx + 1:]
+        elif instruction == 'jmp':
+            yield program[:idx] + [('nop', operand)] + program[idx + 1:]
+
+
 if __name__ == '__main__':
     with open('input', 'r') as program:
         program = [line.split() for line in program.readlines()]
         program = [(instruction, int(operand)) for instruction, operand in program]
 
-    accumulator = run_to_loop(program)
+    try:
+        run(program)
+    except LoopError as e:
+        print(f'Accumulator at first repeated instruction: {e.accumulator}')
 
-    print(f'Accumulator at first repeated instruction: {accumulator}')
+    for patched in patch(program):
+        try:
+            accumulator = run(patched)
+        except LoopError:
+            pass  # that wasn't it…
+        else:
+            print(f'Accumulator using patched program: {accumulator}')
