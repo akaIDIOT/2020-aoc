cadea8861930a598992687a9f407b4db921cc620
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2020-12-19 23:34:34 +0100

    Implement day 10 part 2

--- 10/adapter-array.py
+++ 10/adapter-array.py
@@ -1,4 +1,6 @@
 from collections import Counter
+from itertools import combinations
+from math import prod
 
 
 def connect(adapters):
@@ -10,6 +12,50 @@ def connect(adapters):
     yield 3  # our own device
 
 
+def groups(adapters):
+    current = 0
+    start = current
+    group = []
+
+    for adapter in adapters:
+        if adapter - current == 1:
+            group.append(adapter)
+
+        if adapter - current == 3:
+            yield start, group, adapter
+            start = adapter
+            group = []
+
+        current = adapter
+
+    yield start, group, adapter + 3
+
+
+def possible(start, group, end):
+    current = start
+
+    for adapter in group:
+        if adapter - current > 3:
+            return False
+        current = adapter
+
+    if end - current > 3:
+        return False
+
+    return True
+
+
+def arrangements(start, group, end):
+    if len(group) < 2:
+        return 1
+
+    n = 0
+    for amount in range(1, len(group) + 1):
+        n += sum(possible(start, intermediate, end) for intermediate in combinations(group, amount))
+
+    return n
+
+
 if __name__ == '__main__':
     with open('input', 'r') as adapters:
         adapters = [int(line) for line in adapters]
@@ -17,4 +63,8 @@ if __name__ == '__main__':
 
     diffs = Counter(connect(adapters))
 
-    print(f'All adapters together have {diffs[1]} 1-jolt and {diffs[3]} 3-jolt differences ({diffs[1] * diffs[3]})')
+    print(f'All adapters together have {diffs[1]} 1-jolt and {diffs[3]} 3-jolt differences '
+          f'({diffs[1] * diffs[3]})')
+
+    print(f'Total number of possible arrangements: '
+          f'{prod(arrangements(start, group, end) for start, group, end in groups(adapters))}')
