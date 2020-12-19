8f6a50210f7e2515426361f27d5b5ab91afe2eab
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2020-12-19 20:15:42 +0100

    Implement day 9 part 2

--- 09/encoding-error.py
+++ 09/encoding-error.py
@@ -1,6 +1,15 @@
 preamble_length = 25
 
 
+def sum_target(xmas, start, target):
+    for idx in range(start, len(xmas)):
+        candidate = sum(xmas[start:idx])
+        if candidate == target:
+            return xmas[start:idx]
+        if candidate > target:
+            return None
+
+
 if __name__ == '__main__':
     with open('input', 'r') as xmas:
         xmas = [int(line) for line in xmas]
@@ -12,4 +21,14 @@ if __name__ == '__main__':
             pool.append(n)
         else:
             print(f'Invalid XMAS number: {n}')
+            break
+
+    with open('input', 'r') as xmas:
+        xmas = [int(line) for line in xmas]
+
+    target_number = n
+    for idx in range(len(xmas)):
+        if result := sum_target(xmas, idx, n):
+            result.sort()
+            print(f'Encryption weakness is {result[0]} + {result[-1]}: {result[0] + result[-1]}')
             break
